2e031dadd071d5ec205e18ee08336ee9494285b1
Iryna1511 <148438865+Iryna1511@users.noreply.github.com>
2024-04-29 18:13:17 +0300

    create

---
+++ src/components/App/App.jsx
@@ -0,0 +1,9 @@
+// import { useState } from "react";
+import "./App.css";
+import Description from "../Description/Description";
+
+function App() {
+  return <Description />;
+}
+
+export default App;
